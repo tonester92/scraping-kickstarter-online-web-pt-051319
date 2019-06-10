fefbeee04517f14ffb06ebcfdd442369178d6986
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-10 23:57:34 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,13 @@
-# require libraries/modules here
+require 'pry'
+binding.pry
+
+kickstarter.css("li.project.grid_4").first
+project.css("h2.bbcard_name strong a").text
+project.css("div.project-thumbnail a img").attribute("src").value
+project.css("p.bbcard_blurb").text
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+ 
+kickstarter = Nokogiri::HTML(html)
 end
